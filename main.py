4c1aae870f136e2ab544cd856330f4dd00e93c5f
MoxiMeow <127261425+MoxiMeow@users.noreply.github.com>
2023-03-22 16:23:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+# an encoder/decoder with a shift of 3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# by moxi and lyn
+
+def encoder(password):
+    res = ""
+    for char in password:
+        res += str(int(char) + 3)
+    return res
+
+# moxi's decoder
+def decoder(password):
+    # initialise variables
+    decoded_message = ''
+
+    # adjust and add each character
+    for character in password:
+        decoded_message += str(int(character) - 3)
+
+    # return result
+    return decoded_message
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            raw = input("Please enter your password to encode: ")
+            encoded = encoder(raw)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            decoded = decoder(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {decoded}.")
+        elif option == 3:
+            break
